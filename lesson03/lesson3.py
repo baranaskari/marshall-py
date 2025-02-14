1decf32d6736c3e3424a4ddb3298d2b035e0855e
Baran Askari <baranaskari@TEACH2B-939193L.EDU.YRDSB.NET>
2025-02-14 14:00:59 -0500

    solved lesson 3

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,18 @@
 # Lesson 3
+import math
+side = 0
+side = int(side)
+
+while (side == 0):
+    user = input("Enter number of equal tiles:") 
+    num = float(user)
+
+    if ((num).is_integer()):
+        if ((math.sqrt(num)).is_integer()) :
+            side = int(math.sqrt(num)) 
+            print (f"Side length is {side} ")
+    
+        else: (print("Enter a number that can be square rooted!") )
+
+    else: 
+        print("Please enter a integer") 
