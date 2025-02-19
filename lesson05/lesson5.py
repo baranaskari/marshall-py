bc190f7fa7a5f254dbae043e878fccf2c14bd3f2
Baran <baranaskari86@gmail.com>
2025-02-19 14:20:50 -0500

    lesson 5 solved

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,23 @@
 # Lesson 5
+
+money1 = float(input("Enter the amount of money you have:") )
+cookies = input("Enter the cookie number:") 
+
+big = 0 
+normal = 0 
+
+for check in cookies: 
+    if check == "c": 
+        normal += 1 
+
+    elif check == "b":
+        big += 1  
+    else :
+        print(f"{check} is not a valid input")
+
+total = big + normal 
+profit = ((big * 2) + (normal * 1.25)) - ((big * 0.75) + (normal * 0.50))
+money2 = money1 + profit
+
+print(f"Sold cookies {total} \n Made profit: {profit} \n Now total: {money2}") 
+
