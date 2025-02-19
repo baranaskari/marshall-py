82d34c855c25f5d2852da0ed3ae11d953edb5926
Baran <baranaskari86@gmail.com>
2025-02-19 14:46:04 -0500

    lesson 7 solved

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,14 @@
 # Lesson 7
+import random
+DC = int(input("Enter a DC level: ")) 
+dice = random.randrange(1,21)
+print (f"Ability: {dice}") 
+
+if (DC <= 20) :
+    if (dice >= DC) : 
+        print("Win!") 
+
+    else : 
+        print("Lose!") 
+else :
+    print("DC must be smaller or equal to 20") 
