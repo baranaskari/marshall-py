30dc43c9deee0cd91a102533af062fa3a08053a3
Baran <baranaskari86@gmail.com>
2025-02-20 22:35:12 -0500

    lesson 8 solved

--- lesson08/lesson8.py
+++ lesson08/lesson8.py
@@ -1,1 +1,23 @@
 # Lesson 8
+
+counter = 0 
+
+for i in range(6):
+    current = input(f"Enter the game {i+1} result: ")
+
+    if current == "W":
+        counter += 1
+
+group = 0 
+if counter > 4 : 
+    group = 1
+elif counter > 2 :
+    group = 2
+elif counter > 0 :
+    group = 3
+
+if group == 0: 
+    print("Eliminated!") 
+    
+else:
+    print(f"Player is in group {group}.")
