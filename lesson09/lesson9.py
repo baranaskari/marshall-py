53b44b6ce6ce0ccfa30f7ec859bc02de2f67cefe
Baran <baranaskari86@gmail.com>
2025-02-20 22:48:24 -0500

    lesson 9 solved

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,33 @@
 # Lesson 9
+from random import choice 
+
+invalid = True
+player = ''
+
+while invalid:
+    player = input("Enter R, P or S: ")
+
+    if player in {'R', 'P', 'S'}:
+        invalid = False
+
+cpu = choice(['R', 'P', 'S'])
+print(f"CPU: {cpu}") 
+
+if player == cpu:
+    print("DRAW!")
+else:
+    if player == 'R':
+        if cpu == 'P':
+            print("LOSE!")
+        else:
+            print("WIN!")
+    elif player == 'P':
+        if cpu == 'S':
+            print("LOSE!")
+        else:
+            print("WIN!")
+    else:
+        if cpu == 'R':
+            print("LOSE!")
+        else:
+            print("WIN!")
