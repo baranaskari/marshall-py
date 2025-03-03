3a584514e18969118cc3b7a966fa060d5c6b03a1
Baran <baranaskari86@gmail.com>
2025-03-02 23:00:07 -0500

    lesson 10 solved

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,16 @@
 # Lesson 10
+
+digits = input("Enter last 4 digits: )")
+
+if digit[0] in {'8', '9'}:
+    
+    if digit[-1] in {'8', '9'}:
+        if digits[1] == digits[2]:
+            print(f"The phone number {digits} is a telemarker")
+
+        else:
+            print(f"The phone number {digits} is not a telemarker")
+    else:
+        print(f"The phone number {digits} is not a telemarker")
+else:
+    print(f"The phone number {digits} is not a telemarker")
