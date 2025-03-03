ca7de35b3c5f0bbb479e61d768dc51352d850c59
Baran <baranaskari86@gmail.com>
2025-03-02 23:07:29 -0500

    lesson 11 solved

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,27 @@
 # Lesson 11
+
+coordinate = input("Enter a coordinate: ")
+coordinate = coordinate.split(' ') 
+
+coordinate = list(map(int, coordinate)) 
+
+x, y = coordinate
+
+print(f"x: {x}, y: {y}")
+
+if x > 0 :
+    if y > 0:
+        print(f"At point ({x}, {y}) is in Quadrant 1")
+
+    else:
+        print(f"At point ({x}, {y}) is in Quadrant 4")
+
+else:
+    if y > 0:
+        print(f"At point ({x}, {y}) is in Quadrant 2")
+
+    else:
+        print(f"At point ({x}, {y}) is in Quadrant 3")
+
+
+
