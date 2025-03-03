da755318f14422896556189003058cdeb37271de
Baran <baranaskari86@gmail.com>
2025-03-02 23:22:26 -0500

    lesson 13 solved

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,17 @@
 # Lesson 13
+
+month = int(input("Enter the month num: "))
+day = int(input("Enter the day num: "))
+
+if month == 2 and day == 18 :
+    print("On")
+else:
+    if month < 2: 
+        print("Before")
+    elif month > 2 :
+        print("After")
+    else:
+        if day < 18:
+            print("Before")
+        else:
+            print("After")
