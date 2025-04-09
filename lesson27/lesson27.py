68ccf748acee7c38d10b26372f309631ce99426b
Baran <baranaskari86@gmail.com>
2025-04-09 00:52:51 -0400

    lesson 27 solved

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,13 @@
 # Lesson 27
+
+def string_cleaner(text): 
+    result = ""
+
+    for char in text:
+        if char.isalpha():
+            result += char.lower()
+
+    return result 
+
+value = string_cleaner("hElLo, woRLD!") 
+print(f"value is {value}")
