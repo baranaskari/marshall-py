20fbf1c1ccaf65e26759c06e72a04e7e71228978
Baran <baranaskari86@gmail.com>
2025-04-09 00:48:55 -0400

    lesson 26 solved

--- lesson26/lesson26.py
+++ lesson26/lesson26.py
@@ -1,1 +1,20 @@
 # Lesson 26
+
+def if_div(num1, num2):
+    return num1 % num2 == 0 
+    
+def factor_count(num):
+    counter = 0 
+
+    for divider in range(1, num+1):
+        if if_div(num, divider):
+            counter += 1 
+    
+    return counter 
+
+limit = int(input("enter num"))
+
+for num in range(1, limit+1):
+    factor_size = factor_count(num)
+
+    print(f"{num} has {factor_size} factors.")
