6aa74a27dbba940c4b038fce6678987621b969fa
Baran <baranaskari86@gmail.com>
2025-04-09 00:26:36 -0400

    lesson 22 solved

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,15 @@
 # Lesson 22
+
+limit = int(input("enter num"))
+
+fib_0 = 0
+fib_1 = 1
+fib_n = 0
+
+for n in range(2, limit+1):
+    fib_n = fib_1 + fib_0 
+
+    fib_0 = fib_1 
+    fib_1 = fin_n 
+
+print(f"fibonacci{limit} is {fib_n}.")
