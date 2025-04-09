50585f00595e09ad0d72a4d01efcc44b6f8ce2fe
Baran <baranaskari86@gmail.com>
2025-04-09 09:21:28 -0400

    lesson 36 solved

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,17 @@
 # Lesson 36
+
+def factors(num):
+
+    result = []
+    for div in range(1, num+1):
+        if num % div == 0 : 
+            result.append(div) 
+
+    return result
+
+def prime(num):
+
+    return len(factors(num)) == 2
+
+print(f"factors of 20: {factors(20)}")
+print(f"is 3 prime: {prime(3)}")
