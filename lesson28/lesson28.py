5de6b0238f6bf40b1a6e7cc12fa3c6575e1c380c
Baran <baranaskari86@gmail.com>
2025-04-09 00:59:00 -0400

    lesson 28 solved

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,19 @@
 # Lesson 28
+
+def is_pali(text):
+    if not text:
+        return True
+    elif len(text) < 4:
+        return text[0] == text[-1] 
+    else:
+        mid = len(text) // 2
+
+        for i in range(0, mid):
+            left = text[i]
+            right = text[-1*i - 1]
+
+            if left != right:
+                return False
+        return True 
+
+print(is_pali("tacocat"))
