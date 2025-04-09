daf7289eba34ca15ec48f70d349eb9d7831c79b1
Baran <baranaskari86@gmail.com>
2025-04-09 12:54:40 -0400

    lesson 44 solved

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,16 @@
 # Lesson 44
+
+def c_frequency(word):
+    clean_word = sorted(word.lower())
+    answer = {}
+
+    for character in clean_word:
+        if character not in answer:
+            answer[character] = 1
+        else:
+            answer[character] += 1
+    
+    return answer 
+
+result = c_frequency("Hello")
+print(f"Character Frequency of hello:\n{result}")
