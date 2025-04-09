1bcb6fa68b3fa021048efea34b2131998bd9605f
Baran <baranaskari86@gmail.com>
2025-04-08 23:51:28 -0400

    lesson 15 solved

--- lesson15/lesson15.py
+++ lesson15/lesson15.py
@@ -1,1 +1,14 @@
 # Lesson 15
+
+user_input = input("Search for a fruit. ")
+
+fruits = ["Apple", "Banana", "Kiwi"]
+found = False 
+
+for fruit in fruits:
+    if fruit == user_input:
+        print(f"{user_input} is found!")
+        found = True 
+
+if not found :
+    print(f"{user_input} is not found")  
