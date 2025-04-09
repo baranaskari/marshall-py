9cd8e6dd5e5f9e37d39ac5f7d66f367176f32235
Baran <baranaskari86@gmail.com>
2025-04-09 00:32:06 -0400

    lesson 23 solved

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,23 @@
 # Lesson 23
+
+loop = True
+counter = 0 
+
+while loop:
+    user = input("enter the mark or exit to stop inputting marks: ")
+
+    if user.lower().capitalize() == "Exit":
+        loop = False 
+        break
+    else:
+        mark = int(user)
+        if 0 <= mark <= 100:
+            total += mark
+            counter += 1
+        
+        else:
+            print("Invalid") 
+
+average = total / counter
+
+print(f"your average: {average}") 
