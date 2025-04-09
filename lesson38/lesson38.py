389e496dcae4c2ddb9099feb21e9e93bfd54427e
Baran <baranaskari86@gmail.com>
2025-04-09 11:13:22 -0400

    lesson 38 solved

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,15 @@
 # Lesson 38
+
+def pali(text):
+    return text == text[::-1]
+
+nums = []
+
+for num1 in range(999, 99, -1):
+    for num2 in range(999, 99, -1):
+        current = num1 + num2
+
+        if pali(str(current)):
+            nums.append(current)
+
+print(f"the largest palidrome number in our list is: {max(nums)}")
