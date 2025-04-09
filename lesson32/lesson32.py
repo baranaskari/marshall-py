1f93c3fe28199f0a1a88d97bab9006bcee83bb00
Baran <baranaskari86@gmail.com>
2025-04-09 08:45:49 -0400

    lesson 32 solved

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,19 @@
 # Lesson 32
+
+def duplicate(word1, word2):
+
+    if not word1 or not word2:
+        return []
+    else:
+        set_word1 = set(word1)
+        set_word2 = set(word2)
+
+        result = []
+
+        for char in set_word1:
+            if char in set_word2:
+                result.append(char)
+
+        return sorted(result) 
+
+print(duplicate("hello world", "goodbye world")) 
