f099d68fb794a29d2b3a78a6c928cdfd0d81931c
Baran <baranaskari86@gmail.com>
2025-04-09 08:41:31 -0400

    lesson 31 solved

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,14 @@
 # Lesson 31
+
+def anagram(word1, word2):
+
+    if len(word1) != len(word2):
+        return False 
+    else:
+        for char in word1:
+            if word1.count(char) != word2.count(char):
+                return False
+        
+        return True
+
+print(anagram("bored", "robed"))
