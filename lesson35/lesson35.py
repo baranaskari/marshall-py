5619e13268aa7b395b2b4d3b5716c9e67c038c2f
Baran <baranaskari86@gmail.com>
2025-04-09 09:17:29 -0400

    lesson 35 solved

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,14 @@
 # Lesson 35
+
+def remove_dup(a_list):
+    new_list = []
+
+    for item in a_list:
+        if item not in new_list:
+            new_list.append(item)
+
+    return new_list
+
+test = ["a", "b", "c", "a", "b", "d"]
+print(f"test list: {test}")
+print(f"duplicates removed: {remove_dup(test)}")
