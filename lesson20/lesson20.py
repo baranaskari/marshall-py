f19679f7493367c1a827d073253714f20c6e7c3d
Baran <baranaskari86@gmail.com>
2025-04-09 00:08:51 -0400

    lesson 20 solved

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,16 @@
 # Lesson 20
+
+total = 0
+
+for num in range(1, 10000):
+
+    factor_sum = 0 
+    for div in range(1, num):
+        if num % div == 0:
+            factor_sum += div 
+
+    if factor_sum == num:
+        total += num:
+        print(f"{num} is a perfect number")
+
+print(f"the total sum of all perfect numbers under 10000 is {total}")
