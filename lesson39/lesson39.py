2e65df9a61158e5c5f5c9d8c1c613a88e4adc419
Baran <baranaskari86@gmail.com>
2025-04-09 11:23:31 -0400

    lesson 39 solved

--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,12 @@
 # Lesson 39
+
+def gcd(num1, num2):
+
+    for div in range(min(num1, num2), 0, -1):
+        if num1 % div == 0 and num2 % div == 0:
+            return div
+
+    return 1 
+
+print(f"The GCD of 8 and 12: {gcd(8,12)}")
+print(f"The GCD of 27 and 1080: {gcd(27,1080)}")
