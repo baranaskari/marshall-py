b072e8a21732938281d059c4f46e6331cc240ea9
Baran <baranaskari86@gmail.com>
2025-04-09 01:11:22 -0400

    lesson 30 solved

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,20 @@
 # Lesson 30
+
+def create_line(num):
+
+    text = ""
+    for i in range(1, num+1):
+        if i % 2 == 0:
+            text += "0"
+        else:
+            text += "1"
+    
+    return text 
+
+def output_patter(num):
+
+    for i in range(1, num+1):
+        print(create_line(i))
+
+v = output_patter(5)
+print(v) 
