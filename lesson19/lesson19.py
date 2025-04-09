85ae001478efedf94c1e3f697b76720b2dbd1ded
Baran <baranaskari86@gmail.com>
2025-04-09 00:04:17 -0400

    lesson 19 solved

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,14 @@
 # Lesson 19
+
+num = int(input("enter num"))
+
+counter = 0
+
+for div in range(1, num+1):
+    if num % div == 0:
+        counter +=1 
+
+if counter == 2:
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not prime") 
