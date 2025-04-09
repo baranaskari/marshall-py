ec8c15fe9b0b209a6094db25a0bd7f8439209929
Baran <baranaskari86@gmail.com>
2025-04-09 01:06:23 -0400

    lesson 29 solved

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,24 @@
 # Lesson 29
+
+def constant(text, vowel = False):
+    ctr = 0
+
+    for char in text:
+        char = char.lower()
+        if char.isalpha() and char not in {"a", "e", "i", "o" "u"}:
+            ctr += 1 
+
+    if not vowel:
+        return ctr
+    
+    else:
+        ctr = 0
+
+        for char in text:
+            char = char.lower()
+            if char.isalpha() and char not in {"a", "e", "i", "o" "u"}:
+                ctr += 1 
+        return ctr
+
+print(f"the count is {constant("hello, world!"), True}")
+
