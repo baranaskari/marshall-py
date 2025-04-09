ec59838d5c55d6dac490ea74bb53c0b4c093f27a
Baran <baranaskari86@gmail.com>
2025-04-08 23:56:13 -0400

    lesson 17 solved

--- lesson17/lesson17.py
+++ lesson17/lesson17.py
@@ -1,1 +1,13 @@
 # Lesson 17
+
+num = int(input("enter a value: "))
+
+total = 1
+multiplier = 1
+
+while multiplier <= num :
+    total = total * multiplier 
+
+    multiplier = multiplier +1 
+
+print(f"factorial of {num} is {total}.") 
