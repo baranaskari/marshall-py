ff82f8afa8a5785a5e9536d8e03073182b1af600
Baran <baranaskari86@gmail.com>
2025-04-09 00:37:04 -0400

    lesson 24 solved

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,25 @@
 # Lesson 24
+
+name = ""
+
+longest_length = 0 
+longest_name = ""
+
+while name != "X":
+    name = input("enter name or X to exit")
+
+    if name != "X"
+        current = len(name)
+
+        if current > longest_length:
+            longest_length = current 
+            longest_name = name 
+        
+        else:
+            print("End of the inputs")
+
+if longest_name:
+    print(f"the longest name with {longest_length} characters is {longest_name}")
+else:
+    print("not enough data") 
+
